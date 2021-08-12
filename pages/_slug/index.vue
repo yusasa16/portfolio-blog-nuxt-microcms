b4ce336ccc9c8452344cf6a8b7465fbf0b8e12d5
yusasa16 <yusasa16@gmail.com>
2021-08-12 19:28:49 +0900

    日付 出力文字列変更

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,8 +1,8 @@
 <template>
   <main class="main">
-    <h1 class="title">{{ title }}</h1>
-    <p class="publishedAt">{{ date }}</p>
-    <div class="post" v-html="article"></div>
+    <h1 class="title">{{ microcms.title }}</h1>
+    <p class="publishedAt">{{ microcms.date }}</p>
+    <div class="post" v-html="microcms.article"></div>
   </main>
 </template>
 
@@ -17,7 +17,16 @@ export default {
         headers: { 'X-API-KEY': '5a2b1141-dbfa-4ada-8216-e68624f54486' }
       }
     )
-    return data
-  }
+
+    // 投稿日時・出力用文字列変更
+    let time = Date.parse(data.date);
+    let date = new Date(time);
+    let strDate = `${date.getFullYear()}年${date.getMonth()}月${date.getDate()}日`;
+    data.date = strDate;
+
+    return {
+      microcms: data
+    }
+  },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  BlogCards.mt-10(:blogs="contents")
+  BlogCards.mt-10(:blogs="microcms.contents")
 </template>
 
 <script>
@@ -14,8 +14,20 @@ export default {
         // your-api-key部分は自分のapi-keyに置き換えてください
         headers: { 'X-API-KEY': '5a2b1141-dbfa-4ada-8216-e68624f54486' }
       }
-    )
-    return data
+    );
+
+    // 投稿日時・出力用文字列変更
+    data.contents.forEach(element => {
+      let time = Date.parse(element.date)
+      let date = new Date(time);
+      let strDate = `${date.getFullYear()}年${date.getMonth()}月${date.getDate()}日`;
+      element.date = strDate;
+    });
+
+    // return
+    return {
+      microcms: data
+    }
   }
 }
 </script>
