b590bf91da0bf2fcd43522d5b64962aff1bfbe57
yusasa16 <yusasa16@gmail.com>
2021-08-12 23:03:24 +0900

    ブログカード スタイル調整

--- components/BlogCards.vue
+++ components/BlogCards.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
-  dev.w-10in12.w-screen-lg.mx-auto.flex.justify-between.flex-wrap
+div
+  div.w-10in12.w-screen-lg.mx-auto.flex.justify-between.flex-wrap.after-content
     BlogCard(v-for="blog in blogs" :date="blog.date" :category="blog.category.contentname" :title="blog.title" :id="blog.id")
 </template>
 
@@ -8,3 +9,13 @@ export default {
   props: ['blogs']
 }
 </script>
+
+<style lang="scss" scoped>
+div.after-content {
+  &::after {
+    content: '';
+    display: block;
+    width: calc(1 / 3 * 100%);
+  }
+}
+</style>
