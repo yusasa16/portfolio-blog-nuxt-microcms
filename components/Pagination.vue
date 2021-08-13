132d9aadf42c7fb52a01c82b9737ed1e4a8e8dac
yusasa16 <yusasa16@gmail.com>
2021-08-13 11:42:31 +0900

    ページネーション 追加

---
+++ components/Pagination.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  div
+    ul.flex.max-w-screen-lg.mx-auto.justify-around.md_justify-center
+      li.md_mx-20
+        NuxtLink.block.p-3.bg-gray-200(:to="routingPath + prevPage" :class="{'disabled': !existPrev}") 前
+      li.md_mx-20
+        p.p-3(v-html="currentPage")
+      li.md_mx-20
+        NuxtLink.block.p-3.bg-gray-200(:to="routingPath + nextPage" :class="{'disabled': !existNext}") 次
+
+</template>
+
+<script>
+export default {
+  props: ['currentPage', 'contentsLength'],
+
+  data() {
+    return {
+      prevPage: Number(this.currentPage) - 1,
+      nextPage: Number(this.currentPage) + 1,
+      routingPath: '/page/',
+      existPrev: true,
+      existNext: true
+    }
+  },
+
+  mounted() {
+    if(this.currentPage <= 1) {
+      this.existPrev = false
+    } else {
+      this.existPrev = true
+    }
+
+    this.existNext = this.contentsLength
+
+  console.log(this.contentsLength)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.disabled {
+  pointer-events: none;
+  background-color: #fff;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
-  BlogCards.mt-10(:blogs="microcms.contents")
+  div
+    BlogCards.mt-10(:blogs="microcms.contents")
+    Pagination.mt-10(:currentPage="currentPage" :contentsLength="contentsLength")
 </template>
 
 <script>
@@ -10,10 +12,8 @@ export default {
     const page = params.p || '1'
     const limit = 12
     const { data } = await axios.get(
-      // your-service-id部分は自分のサービスidに置き換えてください
       `https://sakuteck.microcms.io/api/v1/dummy?limit=${limit}&offset=${(page - 1) * limit}`,
       {
-        // your-api-key部分は自分のapi-keyに置き換えてください
         headers: { 'X-API-KEY': '5a2b1141-dbfa-4ada-8216-e68624f54486' }
       }
     );
@@ -25,10 +25,21 @@ export default {
       let strDate = `${date.getFullYear()}年${date.getMonth()}月${date.getDate()}日`;
       element.date = strDate;
     });
+    console.log(data)
+
+    const contentsLength = (data) => {
+      if(data.length < limit) {
+        return false
+      }else{
+        return true
+      }
+    }
 
     // return
     return {
-      microcms: data
+      microcms: data,
+      currentPage: page,
+      contentsLength : contentsLength(data)
     }
   }
 }
