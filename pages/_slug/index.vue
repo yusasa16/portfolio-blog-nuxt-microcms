3aebafd7fbf90af64c7719eea573d417d4c6be1c
yusasa16 <yusasa16@gmail.com>
2021-08-13 09:14:17 +0900

    api取得 変更
    ダミーの記事一覧を取得する

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -12,7 +12,7 @@ import axios from 'axios'
 export default {
   async asyncData({ params }) {
     const { data } = await axios.get(
-      `https://sakuteck.microcms.io/api/v1/blog/${params.slug}`,
+      `https://sakuteck.microcms.io/api/v1/dummy/${params.slug}`,
       {
         headers: { 'X-API-KEY': '5a2b1141-dbfa-4ada-8216-e68624f54486' }
       }
--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ export default {
   async asyncData() {
     const { data } = await axios.get(
       // your-service-id部分は自分のサービスidに置き換えてください
-      'https://sakuteck.microcms.io/api/v1/blog',
+      'https://sakuteck.microcms.io/api/v1/dummy',
       {
         // your-api-key部分は自分のapi-keyに置き換えてください
         headers: { 'X-API-KEY': '5a2b1141-dbfa-4ada-8216-e68624f54486' }
